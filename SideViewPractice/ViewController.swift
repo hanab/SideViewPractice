7b4acdb7f9e87eaffb61192edc40dab570f58272
Hana Demas <hb16826@gmail.com>
2016-08-20 13:35:40 +0300

    Initial Commit

---
+++ SideViewPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideViewPractice
+//
+//  Created by Hana  Demas on 20/08/16.
+//  Copyright © 2016 ___HANADEMAS___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
